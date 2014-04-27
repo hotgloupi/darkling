3168b19ad4b38377eadc0c2ca1fb8f370f174ac2
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-27 22:39:08 +0200

    game: Separate intities creation.

--- game.py
+++ game.py
@@ -1,14 +1,71 @@
 
 from cube import gl, units, gui
-from cubeapp import game
+from cubeapp.game import Game as BaseGame
+from cubeapp.game.entity import component
+from cube.system.inputs import KeySym
 
-class Ground(game.entity.Entity):
+def create_ground(game):
+    mat = gl.Material('ground')
+    mat.ambient = gl.col3f('brown')
+    mat.diffuse = gl.col3f('white')
+    mat.specular = gl.col3f('white')
+    mat.shading_model = gl.material.ShadingModel.gouraud
+    mesh = gl.Mesh()
+    mesh.mode = gl.DrawMode.quads
+    mesh.append(gl.vec3f(-1, 1, 0))
+    mesh.append(gl.vec3f(-1, -1, 0))
+    mesh.append(gl.vec3f(1, -1, 0))
+    mesh.append(gl.vec3f(1, 1, 0))
+    return game.entity_manager.add(
+        'ground',
+        component.Transform,
+        (component.View, {'parent_node': 'transform.node'}),
+        transform = {
+            'transformation':
+                gl.matrix.rotate(
+                    gl.matrix.translate(
+                        gl.matrix.scale(gl.mat4f(), gl.vec3f(10)),
+                        gl.vec3f(0, -1, 0)
+                    ),
+                    units.deg(-90),
+                    gl.vec3f(1, 0, 0)
+                )
+        },
+        view = {
+            'contents': (mat, mesh),
+        },
+    )
 
-    components = [
-        game.entity.component.Transform,
-        (game.entity.component.View, {'parent_node': 'transform.node'})
-    ]
+from cubeapp.game import Controller
+class PlayerController(Controller):
 
+    def __init__(self):
+        pass
+
+def create_player(game):
+    mat = gl.Material('player')
+    mat.ambient = gl.col3f('pink')
+    mat.diffuse = gl.col3f('white')
+    mat.specular = gl.col3f('white')
+    mesh = gl.Spheref(gl.vec3f(0), 1)
+    light = game.renderer.new_light(
+        gl.PointLightInfo(
+            gl.vec3f(0),
+            gl.Color3f("#888"),
+            gl.Color3f("#333"),
+        )
+    )
+    return game.entity_manager.add(
+        'player',
+        component.Transform,
+        (component.View, {'parent_node': 'transform.node'}),
+        view = {
+            'contents': (light, mat, mesh),
+        },
+        transform = {
+            'transformation': gl.matrix.translate(gl.mat4f(), gl.vec3f(0, -3.5, 0))
+        }
+    )
 
 class GameView(gui.widgets.Viewport):
     def __init__(self, game):
@@ -18,33 +75,28 @@ class GameView(gui.widgets.Viewport):
     def render(self, painter2d):
         with self.renderer.begin(gl.mode_3d) as painter:
             painter.state.look_at(
-                gl.vec3f(0, 0, -30), gl.vec3f(0, 0, 0), gl.vec3f(0, 1, 0)
+                gl.vec3f(0, 5, 5), gl.vec3f(0, 0, 0), gl.vec3f(0, 1, 0)
             )
             painter.state.perspective(
                 45, self.game.window.width / self.game.window.height, 0.005, 300.0
             )
+            #print('#' * 40, 'START DRAWING')
             painter.draw([self.game.scene])
+            #print('*' * 40, 'END DRAWING')
 
-class Game(game.Game):
+class Game(BaseGame):
+
+    bindings = {
+        'keyboard': {
+            'up': KeySym.up,
+            'down': KeySym.down,
+            'left': KeySym.left,
+            'right': KeySym.right,
+        },
+    }
 
     def __init__(self, window):
-        super().__init__(window)
+        super().__init__(window, bindings = self.bindings)
         self.view = GameView(self)
-        ground_mat = gl.Material('ground')
-        ground_mat.ambient = gl.col3f('pink')
-        ground_mat.diffuse = gl.col3f('white')
-        ground_mat.specular = gl.col3f('white')
-        ground_mesh = gl.Mesh()
-        ground_mesh.append(gl.vec3f(-1, 1, -1))
-        ground_mesh.append(gl.vec3f(-1, -1, -1))
-        ground_mesh.append(gl.vec3f(1, -1, -1))
-        ground_mesh.append(gl.vec3f(1, 1, -1))
-        self.ground = self.entity_manager.add(
-            Ground,
-            transform = {
-                'transformation': gl.matrix.rotate(gl.mat4f(), units.deg(90), gl.vec3f(1, 0, 0)),
-            },
-            view = {
-                'contents': (ground_mat, ground_mesh),
-            },
-        )
+        self.ground = create_ground(self)
+        self.player = create_player(self)
