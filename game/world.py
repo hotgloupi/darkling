a338b60da46d8fb03d5683af88799630621dd8f4
Raphaël Londeix <raphael.londeix@infinit.io>
2014-08-28 07:39:30 +0200

    world: Subscribe to shutdown event.

--- game/world.py
+++ game/world.py
@@ -3,13 +3,18 @@ import cubeapp.world
 from cubeapp.game.entity import component
 
 class WorldController:
-    channels = ['tick']
 
     def __init__(self, game):
         self.world = cubeapp.world.World(game.renderer)
+        self.referential = cubeapp.world.world.coord_type()
+        self.game = game
+        self.channels = ['tick', game.shutdown_channel]
 
     def __call__(self, ev, delta):
-        pass
+        if ev.channel == self.game.shutdown_channel:
+            self.world.stop()
+            return
+        self.world.update(delta, self.game.camera, self.referential)
 
 def create(game):
     world = game.entity_manager.create("world")
